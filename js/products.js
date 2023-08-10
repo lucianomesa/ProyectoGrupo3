65362e8603fd8b6b800e802c3c4f014e841a0884
Jhonatan Rodriguez <elezerodriguezalvez@hotmail.com>
2023-08-10 02:44:06 -0300

    correccion
    Saque codigo innecesario

---
+++ js/products.js
@@ -0,0 +1,39 @@
+//Para crear éste código nos basamos en la estructura del archivo "categories.js"
+let data_url = "https://japceibal.github.io/emercado-api/cats_products/101.json"
+function setCatID(id) {
+   localStorage.setItem("catID", id);
+   window.location = "product-info.html"
+}
+function showCategoriesList(items){
+    let htmlContentToAppend = "";
+    for(const products of items){
+            htmlContentToAppend += `
+            <div onclick="setCatID(${products.id})" class="list-group-item list-group-item-action cursor-active">
+                <div class="row">
+                    <div class="col-3">
+                        <img src="${products.image}" alt="${products.description}" class="img-thumbnail">
+                    </div>
+                    <div class="col">
+                        <div class="d-flex w-100 justify-content-between">
+                            <h4 class="mb-1">${products.name} - ${products.currency} ${products.cost}</h4>
+                            <small class="text-muted">${products.soldCount} vendidos</small>
+                        </div>
+                        <p class="mb-1">${products.description}</p>
+                    </div>
+                </div>
+            </div>
+            `
+        
+
+        document.getElementById("cat-list-container").innerHTML = htmlContentToAppend;
+    }
+}
+//Primero creamos el fetch y la variable data_url asociada a éste
+async function fetchData(){
+    const response = await fetch(data_url);
+    // console.log(data_url);
+    const data = await response.json();
+    showCategoriesList(data.products);
+    // console.log(data.products)
+}
+fetchData();
