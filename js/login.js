9be6f7dd39a737101f333172d54c2186ac633fef
AbigailTrass <sailorflv@hotmail.com>
2023-08-14 10:16:17 -0300

    Agregue un Párrafo
    Le agregue un parrafo que diga que los datos estan mal

--- js/login.js
+++ js/login.js
@@ -2,13 +2,17 @@ let boton = document.getElementById("ingBtn");
 let checkbox = document.getElementById("terminos");
 let usuario = document.getElementById("usuario");
 let contraseña = document.getElementById("password");
+let parra = document.getElementById('parra');
 
 boton.addEventListener("click", function(){
 if ((usuario.value) && (contraseña.value.length)>=8){
     alert ("Inicio de sesión correcto");
     window.location.href = "index.html";
+    verificar;
 } else {
     alert ("Inicio de sesión incorrecto");
+    parra.innerHTML = "Verifique los datos contraseña o usuario incorrectos"
+    verificar = false
 }
 })
 
