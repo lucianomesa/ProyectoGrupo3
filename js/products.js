8a2db0254ede22ea96377b8538de6418e33b667a
deboragonzalezojeda <deboragonzalezojeda@gmail.com>
2023-08-15 10:48:24 -0300

    Cambios de diseño

--- js/products.js
+++ js/products.js
@@ -1,46 +1,36 @@
-//Para crear éste código nos basamos en la estructura del archivo "categories.js"
-let data_url = "https://japceibal.github.io/emercado-api/cats_products/101.json"
-function setCatID(id) {
-   localStorage.setItem("catID", id);
-   window.location = "product-info.html"
-}
+//URL que contiene los datos
+const URL = 'https://japceibal.github.io/emercado-api/cats_products/101.json';
 
-function titulo(items){
-    let htmlContentToAppend = "";
-    htmlContentToAppend += `
-        <h2>Productos</h2>
-        <p class="lead">Verás aquí todas los productos de categoria <strong>${items.catName}</strong>.</p>
-    `;
-     document.getElementById("containerTitulo").innerHTML = htmlContentToAppend;
-}
+//Variable creada para contener el div 'list-container'
+const listContainer = document.getElementsByClassName('list-container');
 
-function showCategoriesList(items){
-    let htmlContentToAppend = "";
-    for(const products of items){
-            htmlContentToAppend += `
-            <div onclick="setCatID(${products.id})" class="list-group-item list-group-item-action cursor-active">
+//Funcion que hace el fetch de la url
+async function getJsonData(url) {
+    const response = await fetch(url);
+    const data = await response.json();
+    showData(data.products);
+};
+
+getJsonData(URL)
+
+//Funcion que muestra los datos
+function showData(dataArray) {
+    for(const item of dataArray) {
+        listContainer[0].innerHTML += `
+        <div  class="list-group-item list-group-item-action cursor-active">
                 <div class="row">
                     <div class="col-3">
-                        <img src="${products.image}" alt="${products.description}" class="img-thumbnail">
+                    <img src="${item.image}" alt="Imagen del modelo ${item.name}" class="img-thumbnail">
                     </div>
                     <div class="col">
                         <div class="d-flex w-100 justify-content-between">
-                            <h4 class="mb-1">${products.name} - ${products.currency} ${products.cost}</h4>
-                            <small class="text-muted">${products.soldCount} vendidos</small>
+                            <h4 class="mb-1">${item.name} - USD ${item.cost}</h4>
+                            <small class="text-muted">${item.soldCount} artículos</small>
                         </div>
-                        <p class="mb-1">${products.description}</p>
+                        <p class="mb-1">${item.description}</p>
                     </div>
                 </div>
             </div>
-            `;
-        document.getElementById("cat-list-container").innerHTML = htmlContentToAppend;
+        `;
     }
 }
-//Primero creamos el fetch y la variable data_url asociada a éste
-async function fetchData(){
-    const response = await fetch(data_url);
-    const data = await response.json();
-    showCategoriesList(data.products);
-    titulo(data);
-}
-fetchData();
