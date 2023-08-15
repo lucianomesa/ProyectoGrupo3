54866b3eeea19361421b30758809604dc7e6f842
Jhonatan Rodriguez <elezerodriguezalvez@hotmail.com>
2023-08-14 22:15:34 -0300

    Nuevas alertas al login

--- js/login.js
+++ js/login.js
@@ -2,17 +2,23 @@ let boton = document.getElementById("ingBtn");
 let checkbox = document.getElementById("terminos");
 let usuario = document.getElementById("usuario");
 let contraseña = document.getElementById("password");
-let parra = document.getElementById('parra');
+let parra = document.getElementById("alertas");
 
 boton.addEventListener("click", function(){
 if ((usuario.value) && (contraseña.value.length)>=8){
-    alert ("Inicio de sesión correcto");
+    parra.innerHTML += `
+    <div class = "alert alert-success" role = "alert">
+    <p>Datos guardados correctamente</p>
+    <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
+    </div>`;
     window.location.href = "index.html";
     verificar;
 } else {
-    alert ("Inicio de sesión incorrecto");
-    parra.innerHTML = "Verifique los datos contraseña o usuario incorrectos"
-    verificar = false
+    parra.innerHTML = `
+    <div class = "alert alert-danger" role = "alert">
+    <p>Los datos ingresados no cumplen con los requisitos solicitados</p>
+    <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
+    </div>`;
 }
-})
+});
 
