4f57384f58f8a8f518e6f13f2822ffadcf2bdb60
Jhonatan Rodriguez <elezerodriguezalvez@hotmail.com>
2023-08-23 20:10:06 -0300

    Separe el codigo en funciones para que quede mas entendible

--- js/products.js
+++ js/products.js
@@ -1,58 +1,106 @@
 //URL que contiene los datos
-const URL =
-  "https://japceibal.github.io/emercado-api/cats_products/" +
-  localStorage.getItem("catID") +
-  ".json";
+const URL = "https://japceibal.github.io/emercado-api/cats_products/" + localStorage.getItem("catID") + ".json";
 const storedData = JSON.parse(sessionStorage.datos);
-//Variable creada para contener el div 'list-container'
 const listContainer = document.getElementsByClassName("list-container");
+const btnMinMax = document.getElementById("minMax");
+const btnMaxMin = document.getElementById("maxMin");
+const btnRelevancia = document.getElementById("btnRelevancia");
+const buscador = document.getElementById("buscador");
+const filtrar = document.getElementById("rangeFilterCount");
+const rangeFilterCountMin = document.getElementById("rangeFilterCountMin");
+const rangeFilterCountMax = document.getElementById("rangeFilterCountMax");
 
-let btnMinMax = document.getElementById("minMax");
-let btnMaxMin = document.getElementById("maxMin");
-let btnRelevancia = document.getElementById("btnRelevancia");
-let buscador = document.getElementById("buscador");
+// Función para mostrar los productos en el contenedor
+function showProducts(products) {
+  listContainer[0].innerHTML = "";
+  showData(products);
+}
+
+// Función para ordenar productos por precio ascendente
+function sortProductsByPriceAsc(products) {
+  return products.sort((a, b) => a.cost - b.cost);
+}
+
+// Función para ordenar productos por precio descendente
+function sortProductsByPriceDesc(products) {
+  return products.sort((a, b) => b.cost - a.cost);
+}
+
+// Función para ordenar productos por relevancia
+function sortProductsByRelevance(products) {
+  return products.sort((a, b) => b.soldCount - a.soldCount);
+}
+
+// Función para filtrar productos por nombre
+function filterProductsByName(products, searchTerm) {
+  return products.filter((product) =>
+    product.name.toLowerCase().includes(searchTerm)
+  );
+}
+
+// Función para filtrar productos por rango de precio
+function filterProductsByPriceRange(products, min, max) {
+  return products.filter(
+    (product) => product.cost >= min && product.cost <= max
+  );
+}
+
+// Función para limpiar los filtros de rango de precio
+function clearPriceRangeFilter(data) {
+  listContainer[0].innerHTML = "";
+  rangeFilterCountMin.value = "";
+  rangeFilterCountMax.value = "";
+  showData(data);
+}
+
 //Funcion que hace el fetch de la url
 async function getJsonData(url) {
   const response = await fetch(url);
   const data = await response.json();
+  const products = data.products;
   showData(data.products);
-  titulo(data)
-  btnMinMax.addEventListener("click", function () {
-    listContainer[0].innerHTML = "";
-    data.products.sort((a, b) => a.cost - b.cost);
-    showData(data.products);
+  titulo(data);
+
+  btnMinMax.addEventListener("click", function() {
+    showProducts(sortProductsByPriceAsc(data.products));
   });
-  btnMaxMin.addEventListener("click", function () {
-    listContainer[0].innerHTML = "";
-    data.products.sort((aa, bb) => bb.cost - aa.cost);
-    showData(data.products);
+  
+  btnMaxMin.addEventListener("click", function() {
+    showProducts(sortProductsByPriceDesc(data.products));
   });
-  btnRelevancia.addEventListener("click", function () {
-    listContainer[0].innerHTML = "";
-    data.products.sort((aaa, bbb) => bbb.soldCount - aaa.soldCount);
-    showData(data.products);
+  
+  btnRelevancia.addEventListener("click", function() {
+    showProducts(sortProductsByRelevance(data.products));
   });
-  buscador.addEventListener("input", function (e) {
-    listContainer[0].innerHTML = "";
-    let terminoBusqueda = e.target.value.toLowerCase();
-    let productosFiltrados = data.products.filter((producto) => {
-      return producto.name.toLowerCase().includes(terminoBusqueda);
-    });
-    showData(productosFiltrados);
+  
+  buscador.addEventListener("input", function(e) {
+    showProducts(filterProductsByName(data.products, e.target.value.toLowerCase()));
   });
+  
+  filtrar.addEventListener("click", function() {
+    const min = parseInt(rangeFilterCountMin.value);
+    const max = parseInt(rangeFilterCountMax.value);
+    const filteredProducts = filterProductsByPriceRange(data.products, min, max);
+    showProducts(sortProductsByPriceAsc(filteredProducts));
+  });
+  
+  document.getElementById("clearRangeFilter").addEventListener("click", function() {
+    clearPriceRangeFilter(data.products);
+  });
+  
 }
+
 getJsonData(URL);
 
-//Funcion para el nombre 
-function titulo(items){
-    let htmlContentToAppend = "";
-    htmlContentToAppend += `
+//Funcion para mostrar el nombre en pantalla
+function titulo(items) {
+  let htmlContentToAppend = "";
+  htmlContentToAppend += `
         <h2>Productos</h2>
         <p class="lead">Verás aquí todas los productos de categoria <strong>${items.catName}</strong>.</p>
     `;
-     document.getElementById("containerTitulo").innerHTML = htmlContentToAppend;
+  document.getElementById("containerTitulo").innerHTML = htmlContentToAppend;
 }
-
 
 //Funcion que muestra los datos
 function showData(dataArray) {
