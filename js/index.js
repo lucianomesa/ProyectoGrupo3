101cccf6b98b496ebcc6735fd0a2cbab2aeb657e
lucianomesa <lucianomesa1607@gmail.com>
2023-08-23 09:35:27 -0300

    usuario en barra de navegación

--- js/index.js
+++ js/index.js
@@ -11,9 +11,15 @@ document.addEventListener("DOMContentLoaded", function(){
         localStorage.setItem("catID", 103);
         window.location = "products.html"
     });
-
+    let usuario = document.getElementById("navbarUser");
     if(!sessionStorage.datos) {
         alert('Inicia sesion para continuar');
         location.href = "login.html";
-    };
+    }
+    else{
+        const storedData = JSON.parse(sessionStorage.datos);
+        usuario.innerHTML += 
+        `<a class="nav-link" href="index.html">${storedData.email}</a>`
+        ;
+    }
 });
