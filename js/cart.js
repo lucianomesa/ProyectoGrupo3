d034c9879bebc94c6888542e97c582d7a20c385f
Jhonatan Rodriguez <elezerodriguezalvez@hotmail.com>
2023-10-18 14:03:54 -0300

    6

--- js/cart.js
+++ js/cart.js
@@ -16,7 +16,7 @@ document.addEventListener("DOMContentLoaded", function () {
     });
     localStorage.setItem("list", JSON.stringify(updatedList));
   }
-  
+
 
   function addProducts(arr) {
     const productContainer = document.createElement("div");
@@ -26,7 +26,7 @@ document.addEventListener("DOMContentLoaded", function () {
       <div class="col">${arr.name}</div>
       <div class="col d-none d-lg-block d-md-block d-xl-block">${arr.currency} ${arr.unitCost}</div>
       <div class="col ms-auto"><input type="number" class="form-control form-control-sm m-auto contador" min="1" max="100" value="${arr.count}"> <br></div>
-      <div class="col ms-auto" id="total-${arr.id}">${arr.currency}-${arr.unitCost*arr.count}</div>
+      <div class="col ms-auto" id="total-${arr.id}">${arr.currency}-${arr.unitCost * arr.count}</div>
     
     `;
     products.appendChild(productContainer);
@@ -34,62 +34,84 @@ document.addEventListener("DOMContentLoaded", function () {
     const contadorInput = productContainer.querySelector(".contador");
     contadorInput.addEventListener("input", function () {
       agregarTotal(contadorInput.value, arr);
-      
+
     });
   }
 
   async function getJsonData(url) {
     const response = await fetch(url);
     const data = await response.json();
-  
+
     let listCart = JSON.parse(localStorage.getItem("list")) || [];
     const productIdToAdd = data.articles[0].id;
-    const isProductInCart = listCart.some(item => item.id === productIdToAdd); 
+    const isProductInCart = listCart.some(item => item.id === productIdToAdd);
     if (!isProductInCart) {
       listCart.push(data.articles[0]);
       localStorage.setItem("list", JSON.stringify(listCart));
-    } 
+    }
     for (let i = 0; i < listCart.length; i++) {
       addProducts(listCart[i]);
     }
   }
-  getJsonData(urlCart); 
+  getJsonData(urlCart);
 
-  function subTotal(arr){
-   let cont = 0;
-   let unSub = 0;
-   for(let i = 0; i < arr.length; i++){
-    if(arr[i].currency === "UYU"){
-         unSub = (parseInt(arr[i].count)*arr[i].unitCost)/40;
-         cont = cont + unSub;
+  function subTotal(arr) {
+    let cont = 0;
+    let unSub = 0;
+    for (let i = 0; i < arr.length; i++) {
+      if (arr[i].currency === "UYU") {
+        unSub = (parseInt(arr[i].count) * arr[i].unitCost) / 40;
+        cont = cont + unSub;
+      }
+      else {
+        unSub = (parseInt(arr[i].count) * arr[i].unitCost);
+        cont = cont + unSub;
+      }
+
     }
-    else{
-      unSub = (parseInt(arr[i].count)*arr[i].unitCost);
-      cont = cont + unSub;
-    }
-    
-   }
-   return cont;
+    return cont;
   }
   console.log(subTotal(JSON.parse(localStorage.getItem("list"))));
 
   let sub_total = document.getElementById("subTotal");
-  let costSend =  document.getElementById("costSend");
+  let costSend = document.getElementById("costSend");
   let totalTotal = document.getElementById("totalTotal");
 
   sub_total.innerHTML = `USD-${Math.round(subTotal(JSON.parse(localStorage.getItem("list"))))}`;
 
 
-         // Obtén todos los elementos de radio con el atributo "name" igual a "flexRadioDefault"
-         var radioButtons = document.querySelectorAll('input[name="flexRadioDefault"]');
-        
-         // Agrega un evento "change" a cada radio button para detectar cambios
-         radioButtons.forEach(function(radioButton) {
-             radioButton.addEventListener('change', function() {
-                 // Muestra el valor seleccionado en el elemento con id "valorSeleccionado"
-                 costSend.innerHTML = `USD-${Math.round(radioButton.value*subTotal(JSON.parse(localStorage.getItem("list"))))}`; 
-                  totalTotal.innerHTML = `USD-${Math.round(subTotal(JSON.parse(localStorage.getItem("list"))) + radioButton.value*subTotal(JSON.parse(localStorage.getItem("list"))))}`
+  // Obtén todos los elementos de radio con el atributo "name" igual a "flexRadioDefault"
+  var radioButtons = document.querySelectorAll('input[name="flexRadioDefault"]');
 
-             });
-         });
+  // Agrega un evento "change" a cada radio button para detectar cambios
+  radioButtons.forEach(function (radioButton) {
+    radioButton.addEventListener('change', function () {
+      // Muestra el valor seleccionado en el elemento con id "valorSeleccionado"
+      costSend.innerHTML = `USD-${Math.round(radioButton.value * subTotal(JSON.parse(localStorage.getItem("list"))))}`;
+      totalTotal.innerHTML = `USD-${Math.round(subTotal(JSON.parse(localStorage.getItem("list"))) + radioButton.value * subTotal(JSON.parse(localStorage.getItem("list"))))}`
+
+    });
+  });
+
+
+  // Obtener referencias a los campos de tarjeta y cuenta bancaria
+  const creditCardFields = document.querySelectorAll('#creditCardNumber, #securityCode, #expirationDate');
+  const bankTransferFields = document.querySelectorAll('#accountNumber');
+
+  // Agregar eventos de cambio a los botones de radio
+  document.getElementById('creditCardRadio').addEventListener('change', function () {
+    // Si se selecciona "Tarjeta de crédito", habilitar los campos de tarjeta de crédito
+    creditCardFields.forEach(field => field.disabled = !this.checked);
+
+    // Si se selecciona "Tarjeta de crédito", desactivar los campos de transferencia bancaria
+    bankTransferFields.forEach(field => field.disabled = this.checked);
+  });
+
+  document.getElementById('bankTransferRadio').addEventListener('change', function () {
+    // Si se selecciona "Transferencia bancaria", habilitar los campos de transferencia bancaria
+    bankTransferFields.forEach(field => field.disabled = !this.checked);
+
+    // Si se selecciona "Transferencia bancaria", desactivar los campos de tarjeta de crédito
+    creditCardFields.forEach(field => field.disabled = this.checked);
+  });
 });
